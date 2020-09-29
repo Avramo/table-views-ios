46f4ad1b9d07e4727be1dbee2a69975744ec35af
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 14:22:38 +0300

    first commit

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    // array of cell data
+    var cellNameArray = ["Here", "There", "And", "Everywhere", ]
+    
+    // setting number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellNameArray.count
+    }
+    // defining content of cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cellNameArray[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
